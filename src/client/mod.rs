57ae69367fe2901b547286125d885c07f1e78a01
John Van Enk <vanenkj@gmail.com>
2017-09-10 09:57:40 -0700

    Client now has extremely limited console interaction.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,19 +1,20 @@
-use std::io;
-
-use bytes::{BytesMut};
-use futures::{Future};
-use futures::future::ok;
+use bytes::BytesMut;
+use futures::future::{ok};
+use futures::sync::mpsc;
+use futures::{Future, Stream, Sink};
 use message;
+use std::io;
+use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
+use std::thread;
+use std;
 use tokio_core::reactor::Core;
 use tokio_io::codec::Framed;
 use tokio_io::codec::{Encoder, Decoder};
 use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_proto::{TcpClient};
 use tokio_proto::pipeline::{ClientProto};
+use tokio_proto::TcpClient;
 use tokio_service::Service;
-
-use super::{wrap_size, unwrap_size};
 
 #[derive(Debug)]
 pub struct Config {
@@ -28,7 +29,7 @@ impl Encoder for RemnantCodec {
     type Error = io::Error;
 
     fn encode(&mut self, item: message::Request, dst: &mut BytesMut) -> Result<(), io::Error> {
-        wrap_size(&item, dst)
+        super::wrap_size(&item, dst)
     }
 }
 
@@ -37,7 +38,7 @@ impl Decoder for RemnantCodec {
     type Error = io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<message::Response>, io::Error> {
-        unwrap_size(src)
+        super::unwrap_size(src)
     }
 }
 
@@ -56,26 +57,79 @@ impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for RemnantProto {
     }
 }
 
+fn ui_thread(mut ui_tx: mpsc::Sender<String>) -> () {
+    let stdin = std::io::stdin();
+
+    loop {
+        let mut inbuf = String::new();
+        if 0 == stdin.read_line(&mut inbuf).unwrap_or(0) {
+            // stdin closed or we read 0 bytes.
+            break;
+        } else if "done" == inbuf.trim() {
+            break;
+        }
+
+        ui_tx = match ui_tx.send(inbuf).wait() {
+            Ok(x) => {
+                println!("sent");
+                x
+            }
+            Err(e) => {
+                println!("err: {}", e);
+                panic!()
+            }
+        }
+    }
+
+    println!("UI Thread exiting");
+}
+
 pub fn main(cfg: &Config) {
     println!("Remote address: {:?}", cfg.remote_addr);
+
+    let (ui_tx, ui_rx) = mpsc::channel(0);
+    let ui_future = ui_rx.into_future();
+
+    let ui_thread_join = thread::spawn(|| ui_thread(ui_tx));
 
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
+    // let svc:
+
     let client = TcpClient::new(RemnantProto)
         .connect(&cfg.remote_addr, &handle)
         .and_then(|client_service| {
-            client_service
-                .call(message::Request::Ping)
-                .and_then(|r| {
-                    println!("r {:?}", r);
-                    ok(r)
+            // The `client_service` has a type of
+            // `ClientService<TcpStream,RemnantProto>` at this
+            // point. The `Service` trait is implemented for
+            // `ClientService<T,P>`.
+            //
+            // When the service becomes available, we wait for input
+            // from the command line UI. Once that command is
+            // available, we interact with the client_service based on
+            // that input.
+            ui_future
+                .map_err(|(e, r)| {
+                    Error::new(ErrorKind::Other, format!("oh no ({:?},{:?})", e, r))
                 })
-                .map_err(|e| {
-                    println!("err: {:?}", e);
-                    e
+                .and_then(move |(item, _)| {
+                    println!("item {:?}!", item);
+                    client_service
+                        .call(message::Request::Ping)
+                        .and_then(|r| {
+                            println!("r {:?}", r);
+                            ok(r)
+                        })
+                        .map_err(|e| {
+                            println!("err: {:?}", e);
+                            e
+                        })
                 })
         });
 
-    core.run(client).unwrap();
+    let result = core.run(client).unwrap();
+    println!("Main thread done. {:?}", result);
+
+    ui_thread_join.join().unwrap();
 }
